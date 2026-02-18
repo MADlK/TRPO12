cd9caf8b31fd691c4f760792a22ed7117bcab1f5
MADlK <m20eryem03@gmail.com>
2026-02-19 04:44:14 +0500

    Add files via upload

---
+++ WpfApp1/BaseDbService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.DB;
+
+namespace WpfApp1
+{
+    internal class BaseDbService
+    {
+        private BaseDbService ()
+        {
+            context = new DBC();
+        }
+        private static BaseDbService? instance;
+        public static BaseDbService Instance
+        {
+
+            get
+            {
+                if (instance == null)
+                    instance = new BaseDbService();
+                return instance;
+            }
+        }
+        private DBC context;
+        public DBC Context => context;
+    }
+}
---
+++ WpfApp1/DB/DBC.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.DB
+{
+    public class DBC : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=sql.ects;Database=MADBD12;User Id = student_09;" +
+
+            //"Password = student_09;"+
+            //"TrustServerCertificate = True;");
+
+            optionsBuilder.UseSqlServer("Server=localhost;Database=MADBD12;Trusted_Connection=True;" +
+
+            
+            "TrustServerCertificate = True;");
+        }
+    }
+}
